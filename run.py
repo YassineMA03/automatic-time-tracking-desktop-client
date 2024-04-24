91134b52dfece1d017c809722ed34658dbf9af48
YassineMA03 <yassinemadani2003@gmail.com>
2024-04-24 16:30:17 +0200

    Update sauvegarde

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import csv
 import time
 from datetime import datetime, timezone
-
+import pytz
 from helpers import get_active_window_title
 from models import Activity, TimeEntry
 
@@ -9,7 +9,7 @@ from models import Activity, TimeEntry
 def main():
     activities = []
     actual_activity = get_active_window_title()
-    actual_time_entry_start_time = datetime.now(timezone.utc)
+    actual_time_entry_start_time = datetime.now(pytz.timezone("Europe/Paris"))
     last_activity_date = None  # Variable to store the date of the last activity
     print("Resume live:")
     print("")
@@ -18,19 +18,22 @@ def main():
     memoire = set()
     temps_usage_combined = {}
     cnt=6
+    tm=0
     try:
         while True:
             activities, actual_activity, actual_time_entry_start_time = resume_activity(activities,
                                                                                         actual_activity,
                                                                                         actual_time_entry_start_time)
-            
             if len(activities):
                 memoire_generale = set()
                 data=[]
                 # Check if a separator is needed based on the date
-                current_date = datetime.now().date()
+                current_date = datetime.now(pytz.timezone("Europe/Paris")).date()
                 if last_activity_date != current_date:
-                    memoire=set()
+                    activities = []
+                    actual_activity = get_active_window_title()
+                    actual_time_entry_start_time = datetime.now(pytz.timezone("Europe/Paris"))
+                    last_activity_date = None  # Variable to store the date of the last activity
                     cnt+=1
                     cnt%=7
                     separator = ["Activity", "Time spent day : " + current_date.strftime('%Y-%m-%d')]
@@ -57,6 +60,7 @@ def main():
                         csv_writer.writerow([key, value])
 
             time.sleep(1)
+            tm+=1
     except KeyboardInterrupt:
         activities, actual_activity, actual_time_entry_start_time = resume_activity(activities,
                                                                                     actual_activity,
@@ -73,14 +77,14 @@ def resume_activity(activities, actual_activity, actual_time_entry_start_time):
             previous_activity = None
 
         previous_activity_time_entry = TimeEntry(start_time=actual_time_entry_start_time,
-                                                 end_time=datetime.now(timezone.utc))
+                                                 end_time=datetime.now(pytz.timezone("Europe/Paris")))
         if not previous_activity:
             previous_activity = Activity(actual_activity)
             activities.append(previous_activity)
 
         previous_activity.add_time_entry(previous_activity_time_entry)
         actual_activity = current_activity
-        actual_time_entry_start_time = datetime.now(timezone.utc)
+        actual_time_entry_start_time = datetime.now(pytz.timezone("Europe/Paris"))
     return activities, actual_activity, actual_time_entry_start_time
 
 
